1b3b92333221d767dde6d3637623a4f4aa7e169b
Assi Maimon <maimon33@gmail.com>
2018-01-10 09:23:26 +0200

    use conventinal methods

--- aws_ranger.py
+++ aws_ranger.py
@@ -1,70 +1,83 @@
+import re
 import os
 import sys
 import json
 import sched
-import logging
+
 
 from datetime import time, date, timedelta, datetime
 
-import serv
+# import serv
 import boto3
 import click
 
+from wryte import Wryte
 from botocore.exceptions import ClientError
 
-# Setting up a logger
-logger = logging.getLogger('AWS ranger')
-logger.setLevel(logging.INFO)
-console = logging.StreamHandler()
-logger.addHandler(console)
-
-HOME_DIR = '{0}/.aws-ranger'.format(os.getenv("HOME"))
-CONF_DIR = '{0}/aws-creds'.format(HOME_DIR)
-
-def _config():
-    logger.info('Please provide AWS Credentials')
-    AWS_ACCESS_KEY_ID=raw_input('Enter Your AWS Access Key ID : ')
-    AWS_SECRET_ACCESS_KEY=raw_input('Enter Your AWS Secret Access Key : ')
-    AWS_ACCOUNT_ALIAS=raw_input('Enter an Alias for the Account: ')
-    
-    config = {"aws-account": {'AWS_ACCOUNT_ALIAS': AWS_ACCOUNT_ALIAS,
-                              'AWS_ACCESS_KEY_ID': AWS_ACCESS_KEY_ID, 
-                              'AWS_SECRET_ACCESS_KEY': AWS_SECRET_ACCESS_KEY}}
-    with open('{0}/{1}.json'.format(CONF_DIR, AWS_ACCOUNT_ALIAS), 'w') as file:
-        json.dump(config, file, indent=4)
-    
-    global CONFIG_PATH
-    CONFIG_PATH = '{0}/{1}.json'.format(CONF_DIR, AWS_ACCOUNT_ALIAS)
-
-try:
-    if not os.path.exists(CONF_DIR):
-        os.makedirs(CONF_DIR)
-except OSError:
-    logger.warning('Error Creating config folder')
-
-try:
-    if os.listdir(CONF_DIR) == []:
-        raise NameError
-    for file in os.listdir(CONF_DIR):
-        if file.endswith(".json"):
-            global CONFIG_PATH
-            CONFIG_PATH = '{}/{}'.format(CONF_DIR, file)
-        with open(CONFIG_PATH) as config_file:
-            cfg = json.load(config_file)["aws-account"]
-except NameError:
-    logger.info('Needs to be configured first')
-    _config()
-    with open(CONFIG_PATH) as config_file:
-        cfg = json.load(config_file)["aws-account"]
-
-CONFIG_PATH = '{0}/{1}.json'.format(CONF_DIR, cfg['AWS_ACCOUNT_ALIAS'])
-STATE_FILE = '{0}/{1}.state'.format(HOME_DIR, cfg['AWS_ACCOUNT_ALIAS'])
-
+USER_HOME = os.getenv("HOME")
+AWS_RANGER_HOME = '{0}/.aws-ranger'.format(USER_HOME)
+BOTO_CREDENTIALS = '{0}/.aws/credentials'.format(USER_HOME)
 TAGS_EXCLUDE_KEY_WORDS = ["prod", "Production", "do not stop"]
+
+# def _config():
+#     wryter.info('Please provide AWS Credentials')
+#     AWS_ACCESS_KEY_ID=raw_input('Enter Your AWS Access Key ID : ')
+#     AWS_SECRET_ACCESS_KEY=raw_input('Enter Your AWS Secret Access Key : ')
+#     AWS_ACCOUNT_ALIAS=raw_input('Enter an Alias for the Account: ')
+    
+#     config = {"aws-account": {'AWS_ACCOUNT_ALIAS': AWS_ACCOUNT_ALIAS,
+#                               'AWS_ACCESS_KEY_ID': AWS_ACCESS_KEY_ID, 
+#                               'AWS_SECRET_ACCESS_KEY': AWS_SECRET_ACCESS_KEY}}
+#     with open('{0}/{1}.json'.format(CONF_DIR, AWS_ACCOUNT_ALIAS), 'w') as file:
+#         json.dump(config, file, indent=4)
+
+# try:
+#     if os.listdir(CONF_DIR) == []:
+#         raise NameError
+#     for file in os.listdir(CONF_DIR):
+#         if file.endswith(".json"):
+#             global CONFIG_PATH
+#             CONFIG_PATH = '{}/{}'.format(CONF_DIR, file)
+#         with open(CONFIG_PATH) as config_file:
+#             cfg = json.load(config_file)["aws-account"]
+# except NameError:
+#     wryter.info('Needs to be configured first')
+    # _config()
+    # with open(CONFIG_PATH) as config_file:
+    #     cfg = json.load(config_file)["aws-account"]
+
 
 def _format_json(dictionary):
     return json.dumps(dictionary, indent=4, sort_keys=True)
 
+def _yes_or_no(question):
+    while True:
+        reply = str(raw_input(question+' (y/n): ')).lower().strip()
+        if reply[0] == 'y':
+            return True
+        else:
+            wryter.info('Wrong answer. Bye')
+
+def create_config_file(config_path, profile_name="default"):
+    TEXT = """Please enter tag values to exclude
+        them from the aws-ranger (please comma to seperate them)
+        [prod, production, do not stop, keep]:"""
+    EXCLUDE_TAGS = raw_input(TEXT) or ["prod", "production", "do not stop", "keep"]
+    print EXCLUDE_TAGS
+    with open(config_path, 'w') as file:
+        EXCLUDE_TAGS = raw_input("Please enter tag values to exclude\
+        them from the aws-ranger (please comma to seperate them)\
+        [prod, production, do not stop, keep]:") or ["prod", "production", "do not stop", "keep"]
+        print EXCLUDE_TAGS
+    pass
+
+def find_profiles(file):
+    profiles_list = []
+    boto_config = open(file).read()
+    for match in re.findall("\[.*\]", boto_config):
+        profiles_list.append(match.strip("[]"))
+    return profiles_list
+        
 def create_short_instances_dict(all_instances_dictionary):
     instance_dict ={}
 
@@ -84,28 +97,29 @@ def create_short_instances_dict(all_instances_dictionary):
                     instance_dict[region[0]] = instances_ids_list
     return instance_dict
 
-class aws_ranger():    
-    def __init__(self):
-        ACCESS_KEY = cfg['AWS_ACCESS_KEY_ID']
-        self.ACCESS_KEY = ACCESS_KEY
-
-        SECRET_KEY = cfg['AWS_SECRET_ACCESS_KEY']
-        self.SECRET_KEY = SECRET_KEY
+class aws_ranger():
+    def __init__(self, profile_name):
+        wryter = Wryte(name='aws-ranger')
+        try:
+            self.aws_client(resource=False, 
+                            profile_name=profile_name).describe_regions()
+        except ClientError as e:
+            wryter.info('Failed to Authenticate your AWS account\n'
+            'Review your boto credentials file at ~/.aws/credentials')
+            sys.exit()
 
     def aws_client(self, 
                    resource=True, 
+                   profile_name='default',
                    region_name='eu-west-1', 
                    aws_service='ec2'):
+        
+        session = boto3.Session(profile_name=profile_name)
+
         if resource:
-            return boto3.resource(aws_service,
-                                    aws_access_key_id=self.ACCESS_KEY,
-                                    aws_secret_access_key=self.SECRET_KEY,
-                                    region_name=region_name)
+            return session.resource(aws_service, region_name=region_name)
         else:
-            return boto3.client(aws_service,
-                                aws_access_key_id=self.ACCESS_KEY,
-                                aws_secret_access_key=self.SECRET_KEY,
-                                region_name=region_name)
+            return session.client(aws_service, region_name=region_name)
         
     def _get_all_regions(self):
         region_list = []
@@ -169,17 +183,20 @@ class aws_ranger():
         pass
 
     def start_instnace(self, instance_list, region=False):
-        for instance in instance_list:            
+        for instance in instance_list:
+            wryter.info('Starting instance: {}'.format(instance))
             self.aws_client(region_name=region).instances.filter(
                 InstanceIds=instance).start()
 
     def stop_instnace(self, instance_list, region=False):
-        for instance in instance_list:            
+        for instance in instance_list:
+            wryter.info('Stopping instance: {}'.format(instance))
             self.aws_client(region_name=region).instances.filter(
                 InstanceIds=instance).stop()
 
     def terminate_instnace(self, instance_list, region=False):
         for instance in instance_list:
+            wryter.info('Terminating instance: {}'.format(instance))
             self.aws_client(region_name=region).instances.filter(
                 InstanceIds=instance_list).terminate()
 
@@ -210,6 +227,9 @@ CLICK_CONTEXT_SETTINGS = dict(
 @click.group(invoke_without_command=True, 
              context_settings=CLICK_CONTEXT_SETTINGS)
 @click.pass_context
+@click.option('--init',
+              is_flag=True,
+              help="Config aws-ranger for first use")
 @click.option('-v',
               '--verbose',
               is_flag=True,
@@ -218,60 +238,75 @@ CLICK_CONTEXT_SETTINGS = dict(
               '--debug',
               is_flag=True,
               help="debug new features")
-def ranger(ctx, verbose, debug):
+def ranger(ctx, init, verbose, debug):
     """Round up your AWS instances
 
     Scout for Instances in all AWS Regions
     """
-    ranger = aws_ranger()
+    if verbose:
+        wryter = Wryte(name='wryte', level='debug')
+        
+    DEFAULT_AWS_PROFILE = find_profiles(BOTO_CREDENTIALS)[0]
+    CONFIG_PATH = '{0}/{1}.conf'.format(AWS_RANGER_HOME,
+                                        DEFAULT_AWS_PROFILE)
+    STATE_FILE = '{0}/{1}.state'.format(AWS_RANGER_HOME,
+                                        DEFAULT_AWS_PROFILE)
+    ctx.obj = [CONFIG_PATH, STATE_FILE]
+    
+    wryter = Wryte(name='aws-ranger')
+
+    if init:
+        confirm = 'You are about to create Home dir for aws-ranger.\n'
+        'Continue?'
+        if os.path.exists(AWS_RANGER_HOME):
+            wryter.info('aws-ranger was already initiated')
+            sys.exit()
+
+        if _yes_or_no(confirm):
+            os.makedirs(AWS_RANGER_HOME)
+            create_config_file(CONFIG_PATH, DEFAULT_AWS_PROFILE)
+            sys.exit()
+        else:
+            sys.exit()
+    
+    if not os.path.exists(AWS_RANGER_HOME):
+        wryter.info('Missing aws-ranger HOME dir\n'
+        'Run `aws-ranger --config` or create it yourself at ~/.aws-ranger')
+        sys.exit()
+    
+    ranger = aws_ranger(profile_name='default')
     
     if debug:
-        # timer = scheduler()
-        # print timer.get_seconds_difference(timer.END_OF_DAY)
-        ranger.create_state_file(ranger.get_instances())
+        create_config_file(CONFIG_PATH, DEFAULT_AWS_PROFILE)
+        # ranger.create_state_file(ranger.get_instances())
         sys.exit()
-    if verbose:
-        logger.setLevel(logging.DEBUG)
     
     if ctx.invoked_subcommand is None:
         instances = create_short_instances_dict(ranger.get_instances())
-        # print instances
-    else:
-        pass
+        print instances
 
 @ranger.command('stop')
+@click.pass_obj
 @click.argument('region', default=False)
-@click.option('-s',
-              '--server',
-              is_flag=True,
-              help='Send the Ranger to Background')
-def stop(region, server):
+def stop(ctx, region):
     """Stop instances Found by aws-ranger
     """
-    ranger = aws_ranger()
-    if server:
-        # TODO: Impliment serv machenizem
-        logger.info('Still not working')
-        sys.exit()
+    CONFIG_PATH = ctx[0]
+    ranger = aws_ranger(profile_name='default')
+
     instances = ranger.get_instances()
     stop_list = create_short_instances_dict(instances)
     for k, v in stop_list.items():
         ranger.stop_instnace(v, region=k)
 
 @ranger.command('terminate')
+@click.pass_obj
 @click.argument('region', default=False)
-@click.option('-s',
-              '--server',
-              is_flag=True,
-              help='Send the Ranger to Background')
-def terminate(region, server):
+def terminate(region):
     """Terminate instances Found by aws-ranger
     """
-    ranger = aws_ranger()
-    if server:
-        # TODO: Impliment serv machenizem
-        logger.info('Still not working')
-        sys.exit()
+    ranger = aws_ranger(profile_name='default')
+
     instances = ranger.get_instances()
     stop_list = create_short_instances_dict(instances)
     for k, v in stop_list.items():
@@ -281,3 +316,21 @@ def terminate(region, server):
     stop_list = create_short_instances_dict(instances)
     for k, v in stop_list.items():
         ranger.terminate_instnace(v, region=k)
+
+@ranger.command('deamon')
+@click.pass_obj
+@click.argument('region', default=False)
+@click.option('-a',
+              '--alert',
+              is_flag=True,
+              help='No Action, Just alert by mail')
+def deamon(region):
+    wryter = Wryte(name='aws-ranger')
+
+    # Checks you are running with sudo privileges
+    if os.getuid() != 0:
+        wryter.info('You run with sudo privileges to run a deamon')
+        sys.exit()
+
+    ranger = aws_ranger(profile_name='default')
+    pass
